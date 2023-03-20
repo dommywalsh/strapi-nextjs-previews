8c0ed695c8fdfbf999b7223bb1e244c736c64495
Dominic Walsh <djcwalsh94@gmail.com>
2023-03-20 08:21:49 +0200

    experimenting with new preview feature

---
+++ backend/src/admin/webpack.config.js
@@ -0,0 +1,16 @@
+// ./src/admin/webpack.config.js
+'use strict';
+/* eslint-disable no-unused-vars */
+module.exports = (config, webpack) => {
+  // Note: we provide webpack above so you should not `require` it
+  // Perform customizations to webpack config
+  // Important: return the modified config
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      CLIENT_FRONTEND_URL: JSON.stringify(process.env.CLIENT_FRONTEND_URL),
+      CLIENT_PREVIEW_SECRET: JSON.stringify(process.env.CLIENT_PREVIEW_SECRET),
+      FRONT_END_URL: JSON.stringify(process.env.FRONT_END_URL),
+    })
+  )
+  return config;
+};
